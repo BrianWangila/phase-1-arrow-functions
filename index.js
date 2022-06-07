b1fb59509e1d54305d786d3449c54b2423281b4b
Brian Wangila <bwanjalah@gmail.com>
2022-06-08 01:16:12 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+let divide = function (){
+  return 2000 / 100
+}
+console.log(divide());
+
+let square = parameter => parameter ** 2
+console.log(square(20));
+
+let add = (para1, para2) => para1 + para2
+console.log(add(10, 43));
